b193b3b92d883c720069f890bf559e1f3792ad3a
Nguyen Vinh Thao <nguyenvinhthao@MacBook-Pro-cua-Nguyen.local>
2023-02-17 18:04:45 +0700

    update

--- src/pages/movie-detail/components/detail/Detail.jsx
+++ src/pages/movie-detail/components/detail/Detail.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchMovieDetailApi } from "../../../../services/movie";
+import Trailer from "../../../home/components/trailer/Trailer";
+import "./index.scss";
 
 export default function Detail() {
   const [movieDetail, setMovieDetail] = useState({});
@@ -20,25 +22,25 @@ export default function Detail() {
   };
   return (
     <div className="row">
-      <div className="col-6">
+      <div className="col-12">
         <div className="row">
-          <div className="col-6">
-            <img src={movieDetail.hinhAnh} alt="" />
+          <div className="col-lg-4 col-md-12">
+            <img width={320} height={420} src={movieDetail.hinhAnh} alt="" />
           </div>
-          <div className="col-6">
-            <h3>{movieDetail.tenPhim}</h3>
-            <h5>{movieDetail.danhGia}</h5>
-            <h4>Nội dung</h4>
-            <p>{movieDetail.moTa}</p>
+          <div className="title col-lg-8 col-md-12">
+            <h1>{movieDetail.tenPhim}</h1>
+            <h5>Đánh giá: {movieDetail.danhGia}/10</h5>
+            <h3>Nội dung:</h3>
+            <p >{movieDetail.moTa}</p>
           </div>
         </div>
       </div>
-      <div className="col-6">
-        <h4 className="mb-3">Trailer</h4>
+      <div className="trailer mt-5 col-12">
+        <h4 className="mb-3">Trailer:</h4>
         {/* <video width="480" height="320" controls poster='https://movienew.cybersoft.edu.vn/hinhanh/hanh-phuc-mau_gp03.jpeg'>
                     <source src="https://youtu.be/0rEKdTK1igo" type="video/mp4" />
                 </video> */}
-        <div width="320" height="520">
+        {/* <div>
           <div className="card card-movie">
             <img
               className="card-img-top"
@@ -50,7 +52,8 @@ export default function Detail() {
               </button>
             </div>
           </div>
-        </div>
+        </div> */}
+        <Trailer />
       </div>
     </div>
   );
