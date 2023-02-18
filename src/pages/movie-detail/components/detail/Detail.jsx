9afb8c4b249352e2a5b15a37701fe2fcac017802
Nguyen Vinh Thao <nguyenvinhthao@MacBook-Pro-cua-Nguyen.local>
2023-02-18 20:55:36 +0700

    updatetrailer

--- src/pages/movie-detail/components/detail/Detail.jsx
+++ src/pages/movie-detail/components/detail/Detail.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchMovieDetailApi } from "../../../../services/movie";
+import Trailer from "../../../../components/trailer/Trailer";
 
 import "./index.scss";
 
@@ -27,36 +28,26 @@ export default function Detail() {
           <div className="image col-md-12 col-lg-4">
             <img width={320} height={480} src={movieDetail.hinhAnh} alt="" />
           </div>
-          <div className="title col-md-12 col-lg-6">
+          <div className="title pl-5 col-md-12 col-lg-4">
             <h1>{movieDetail.tenPhim}</h1>
             <h5>Đánh giá: {movieDetail.danhGia}/10
-            <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
             </h5>
             <h3>Nội dung:</h3>
             <p >{movieDetail.moTa}</p>
           </div>
-        </div>
-      </div>
-      <div className="trailer mt-5 col-12">
-        <h4 className="mb-3">Trailer:</h4>
-        {/* <video width="480" height="320" controls poster='https://movienew.cybersoft.edu.vn/hinhanh/hanh-phuc-mau_gp03.jpeg'>
-                    <source src="https://youtu.be/0rEKdTK1igo" type="video/mp4" />
-                </video> */}
-        {/* <div>
-          <div className="card card-movie">
-            <img
-              className="card-img-top"
-              src="https://movienew.cybersoft.edu.vn/hinhanh/hanh-phuc-mau_gp03.jpeg"
-            />
-            <div className="overlay d-flex justify-content-center align-items-center">
-              <button className="button-movie-item mx-1 show-trailer">
-                <i className="fa fa-play"></i>
-              </button>
+          <div className="trailer col-lg-4">
+          <h4 className="mb-3">Trailer:</h4>
+          <div className="trailer-inner">
+            <div className="trailer-wrapper">
+              <Trailer />
             </div>
           </div>
-        </div> */}
+        </div>
+        </div>
         
       </div>
+
     </div>
   );
 }
