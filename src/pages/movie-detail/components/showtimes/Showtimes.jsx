8189c8dd8763609f3a63784912cee342243f2fb7
Nguyen Vinh Thao <nguyenvinhthao@MacBook-Pro-cua-Nguyen.local>
2023-02-27 11:04:30 +0700

    theater

--- src/pages/movie-detail/components/showtimes/Showtimes.jsx
+++ src/pages/movie-detail/components/showtimes/Showtimes.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { fetchMovieShowtimesApi } from '../../../../services/cinema'
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { fetchMovieShowtimesApi } from '../../../../services/cinema';
+import moment from 'moment';
+import "./index.scss";
+
 
 export default function Showtimes() {
     const [movieShowtimes, setMovieShowtimes] = useState({});
@@ -37,26 +40,25 @@ export default function Showtimes() {
                     {ele?.cumRapChieu?.map(ele => {
                         return (
                             <div key={ele.maCumRap} className="row mb-5">
-                                <div className="col-1">
+                                <div className="col-2">
                                     <img className="img-fluid rounded" src={ele.hinhAnh} />
                                 </div>
-                                <div className="col-11 pl-0">
+                                <div className="col-10 pl-0 info">
                                     <h5>{ele.tenCumRap}</h5>
                                     <span className="text-muted">{ele.diaChi}</span>
-                                </div>
-                                <div className="col-12">
                                     <div className="row">
                                         {
                                             ele?.lichChieuPhim?.map((ele) => {
                                                 return (
-                                                    <div key={ele.maLichChieu} className="col-3">
-                                                        <Link to={`/booking/${ele.maLichChieu}`}>{ele.ngayChieuGioChieu}</Link>
+                                                    <div key={ele.maLichChieu} className="col-3 mt-2">
+                                                        <Link to={`/booking/${ele.maLichChieu}`}>{moment(ele.ngayChieuGioChieu).format("llll")}</Link>
                                                     </div>
                                                 );
                                             })
                                         }
                                     </div>
                                 </div>
+                                
                             </div>
                         )
                     })}
@@ -67,12 +69,12 @@ export default function Showtimes() {
     return (
         <div className="col-12 mt-5">
             <div className="row">
-                <div className="col-3">
-                    <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <div className="col-12">
+                    <div className="nav mb-5 nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                         {renderTabs()}
                     </div>
                 </div>
-                <div className="col-9">
+                <div className="col-12">
                     <div className="tab-content" id="v-pills-tabContent">
                         {renderTabcontents()}
 
